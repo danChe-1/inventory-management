274612f094ff459b013e70cf1ada114806e88024
Daniel <69362391+danChe-1@users.noreply.github.com>
2024-09-05 22:20:24 +0300

    Update next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "https://s3invmanagement.s3.eu-central-1.amazonaws.com",
+        hostname: "s3invmanagement.s3.eu-central-1.amazonaws.com",
         port: "",
         pathname: "/**",
       },
